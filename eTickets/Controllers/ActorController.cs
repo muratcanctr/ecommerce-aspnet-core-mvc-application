9d1dbb4350e7de8c54e66f06bf858723534a7f2c
Murat Can Çotur <39571221+muratcanctr@users.noreply.github.com>
2022-07-30 20:45:54 +0300

    Adding Controllers

---
+++ eTickets/Controllers/ActorController.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class ActorController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View();
+        }
+    }
+}
---
+++ eTickets/Controllers/CinemaController.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemaController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
