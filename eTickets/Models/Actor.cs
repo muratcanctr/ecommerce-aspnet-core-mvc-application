dd1eccbb67a75335d8fd8977c6f492af01e12174
Murat Can Çotur <39571221+muratcanctr@users.noreply.github.com>
2022-07-30 16:45:54 +0300

    Database transactionsModels

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int ActorId { get; set; }
+
+
+        public string ProfilePictureURL { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Actor_Movie> Actors_Movies { get; set; }
+    }
+}
---
+++ eTickets/Models/Producer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int ProducerId { get; set; }
+
+
+        public string ProfilePictureURL { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+
+        //Relationships
+        public List<Movie> Movies { get; set; }
+
+
+    }
+}
