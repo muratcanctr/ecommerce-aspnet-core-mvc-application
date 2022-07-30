c1351d1c08b8c6505b01acf27f5669998e379024
Murat Can Çotur <39571221+muratcanctr@users.noreply.github.com>
2022-07-30 20:23:12 +0300

    Fix

--- eTickets/Models/Movie.cs
+++ eTickets/Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using eTickets.Data;
+using eTickets.Data.Enums;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace eTickets.Models
